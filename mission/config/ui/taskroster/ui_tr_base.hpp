c5c1ebd202eb6c505b709731c8fc9ec1841aa7bd
dijksterhuis <11841332+dijksterhuis@users.noreply.github.com>
2024-07-22 12:08:01 +0100

    TaskRoster: Connect new teams and tasks pages to taskroster UI config.

--- mission/config/ui/taskroster/ui_tr_base.hpp
+++ mission/config/ui/taskroster/ui_tr_base.hpp
@@ -6,6 +6,10 @@
 
 
 //Switch Teams (can't remember, why i made it an extra Display /shrug)
+// @dijksterhuis: ^ probably so that the left hand side of the task roster
+// can be redrawn as well as right hand side.
 #include "ui_tr_display_selectTeam.hpp"
+#include "ui_tr_display_showTeamInfo.hpp"
+#include "ui_tr_display_showTasksInfo.hpp"
 //Main Menu
 #include "ui_tr_display_main.hpp"
